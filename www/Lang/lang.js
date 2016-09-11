0be064434bf50ca3f071a62a6e8de731f9a2cacf
Sayon <sayonadhikary2012@gmail.com>
2016-09-11 15:38:03 +0530

    BoomBoom App

---
+++ www/Lang/lang.js
@@ -0,0 +1,6 @@
+lang = {
+    confirmMsg: {
+        playlist: "Are you sure you want to delete this video from playlist?",
+        clearPlaylist: "Are you sure you want to delete all videos from the playlist?"
+    }
+}
